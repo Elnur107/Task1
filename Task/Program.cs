2fd164c7da433d332807bdef1653ca88f40e1844
AkberliElnur <126491959+Elnur107@users.noreply.github.com>
2023-12-22 12:33:03 +0300

    Add files via upload

---
+++ Task/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Program.Models;
+
+class Program
+{
+    static void Main()
+    {
+        User user1 = new User("John Doe", "john.doe@example.com", "P1s@$#@!");
+
+        user1.ShowInfo();
+
+        string newPassword = "NewP@ssword";
+        bool isValidPassword = user1.PasswordChecker(newPassword);
+        Console.WriteLine($"Is the password valid? {isValidPassword}");
+    }
+}
---
+++ Task/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Program.Models;
+
+public interface IAccount
+{
+    bool PasswordChecker(string password);
+    void ShowInfo();
+}
+
+public class User : IAccount
+{
+    private static int _idCounter = 1;
+
+    public int Id { get; }
+    public string Fullname { get; }
+    public string Email { get; }
+    private string Password { get; }
+
+    public User(string fullname, string email, string password)
+    {
+        Id = _idCounter++;
+        Fullname = fullname;
+        Email = email;
+
+        if (IsValidPassword(password))
+            Password = EncryptPassword(password);
+        else
+            throw new ArgumentException("Invalid password");
+    }
+
+    public bool PasswordChecker(string password)
+    {
+        return IsValidPassword(password);
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"ID: {Id}\nFullname: {Fullname}\nEmail: {Email}");
+    }
+
+    private bool IsValidPassword(string password)
+    {
+        return password.Length >= 8 &&
+               password.Any(char.IsUpper) &&
+               password.Any(char.IsLower) &&
+               password.Any(char.IsDigit);
+    }
+
+    private string EncryptPassword(string password)
+    {
+        return password;
+    }
+}
+
